2615c00148c0223e7ebf118e092c8314794ce3b3
Bishwas Bhandari <bishwasbh@gmail.com>
2025-03-18 10:35:21 +0545

    chore: love thy eyes

--- main.go
+++ main.go
@@ -1,14 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"github.com/Bishwas-py/notify"
+	"github.com/godbus/dbus/v5"
+	"log"
 	"path"
 	"runtime"
+	"time"
 )
 
-func main() {
-	logo := "logo.png"
+const AppID = "Love Thy Eyes"
+
+var AppIcon string
+
+func init() {
 	_, filename, _, _ := runtime.Caller(0)
-	logoDir := path.Join(path.Dir(filename), logo)
-	notify.Notify("app name", "notice", "some text", logoDir)
+	AppIcon = path.Join(path.Dir(filename), "logo.png")
+}
+
+func main() {
+	hints := map[string]dbus.Variant{
+		"body-markup":     dbus.MakeVariant(true), // Enable markup in body
+		"body-hyperlinks": dbus.MakeVariant(true), // Enable hyperlinks specifically
+	}
+
+	notification := notify.Notification{
+		Title:   "Now, take a break!",
+		AppIcon: AppIcon,
+		AppID:   AppID,
+		Body:    "Go out, see something beautiful, something far away, a mountain, a river, a forest or a sea. It will help you to relax your eyes.",
+		Actions: notify.Actions{
+			{
+				Title: "Hello",
+				Trigger: func() {
+					log.Println("Hello")
+				},
+			},
+		},
+		Hints:   hints,
+		Timeout: int(10 * time.Second),
+	}
+
+	_, s := notification.Actions.Results()
+	fmt.Printf("%v", s)
+
+	_, _ = notification.Trigger()
 }
